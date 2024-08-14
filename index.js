4c29243a680253c3c940b34de011dba5af7f550e
Alpes <iaalvearp@gmail.com>
2024-08-14 01:58:58 -0500

    Adding interaction to Photo section elements

--- index.js
+++ index.js
@@ -1,29 +1,2 @@
 import 'https://flackr.github.io/scroll-timeline/dist/scroll-timeline.js';
 
-
-/*const ids = ['photoSection', 'dateSection', 'directionSection', 'dressCode', 'footerSection', 'itinerarySection', 'pictureSection']
-
-const scrollingPics = document.querySelectorAll('.scrollingPics')
-
-
-
-scrollingPics.forEach(pic => {
-    const sectionTimeline = new ScrollTimeline({
-        scrollOfsets: [
-            { target: pic, edge: "end", threshold: "1" },
-            { target: pic, edge: "start", threshold: "0" },
-        ],
-    })
-
-    pic.animate(
-        {
-            transform: ["translateY(5rem) scale(.5)", "translateY(0) scale(1)"],
-            opacity: ['0.5', '1'],
-        },
-        {
-            duration: 1,
-            timeline: sectionTimeline
-        }
-    )
-})
-*/
